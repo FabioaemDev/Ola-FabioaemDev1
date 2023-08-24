cb9b83247b9093b8ff47b29bf167c5775c6b45f4
FabioaemDev <fabioehrhardtdev@gmail.com>
2023-08-24 15:58:20 -0300

    Nem sei o que foi isso
    Eu teclei F5 no VS Code pra executar o código, e abriu esse arquivo

---
+++ site-exemplo/teste1.js
@@ -0,0 +1,8 @@
+var N1, N2, S;
+
+
+N1 = Number(window.prompt('Digite a sua 1ª Nota'));
+N2 = Number(window.prompt('Digite a sua 2ª Nota'));
+window.alert(['Soma entre ',N1,' e ',N2,' é igual a ',N1 + N2].join(''));
+S = N1 + N2;
+window.alert(S / 2);
