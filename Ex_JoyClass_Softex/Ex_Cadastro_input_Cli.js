7d29c118e3b617c50f070d943d047cc7b4d4525c
FabioaemDev <fabioehrhardtdev@gmail.com>
2023-09-19 11:06:42 -0300

    Exercício softex
    Exercício plataforma Softex Lógica de programação. Cadastro com exibição das informções impultadas pelo usuário

---
+++ Ex_JoyClass_Softex/Ex_Cadastro_input_Cli.js
@@ -0,0 +1,5 @@
+var cidade, cpf, Endereco, Idade, Nome, Nome_do_pai, Nome_da_mae, Peso, Renda_Bruta, RG;
+
+Nome = window.prompt("Digite seu Nome");
+Idade = window.prompt('digite sua idade');
+cpf = window.prompt('Ditite seu CPF');
